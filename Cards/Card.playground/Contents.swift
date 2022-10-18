f00238421ee61bbebcf946f9671470489b9ffb73
Uladzislau_Komar <vladkomar25@gmail.com>
2022-10-18 19:48:40 +0300

    Prepared backsidecircle

--- Cards/Card.playground/Contents.swift
+++ Cards/Card.playground/Contents.swift
@@ -48,12 +48,54 @@ class SquareShape: CAShapeLayer, ShapeLayerProtocol {
     }
 }
 
+class FillShape: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        let path = UIBezierPath(rect: rect)
+        path.close()
+        self.path = path.cgPath
+        self.fillColor = fillColor
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("You can't use initial init")
+    }
+}
+
+class BackSideCircle: CAShapeLayer, ShapeLayerProtocol {
+    required init(size: CGSize, fillColor: CGColor) {
+        super.init()
+        let path = UIBezierPath()
+        
+        for _ in 1...15 {
+            let randomX =  Int.random(in: 1...Int(size.width))
+            let randomY = Int.random(in: 1...Int(size.height))
+            let center = CGPoint(x: randomX, y: randomY)
+            let radius = Int.random(in: 5...15)
+            
+            path.move(to: center)
+            path.addArc(withCenter: center, radius: CGFloat(radius), startAngle: 0, endAngle: .pi*2, clockwise: true)
+        }
+        
+        self.path = path.cgPath
+        self.strokeColor = fillColor
+        self.fillColor = fillColor
+        self.lineWidth = 1
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("You can't use initial init")
+    }
+}
+
 class MyViewController : UIViewController {
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
         self.view = view
-        view.layer.addSublayer( SquareShape(size: CGSize(width: 70, height: 150), fillColor: UIColor.red.cgColor) )
+        view.layer.addSublayer( BackSideCircle(size: CGSize(width: 150, height: 150), fillColor: UIColor.red.cgColor) )
     }
 }
 // Present the view controller in the Live View window
