d84d1036a1b6cbe5340d6bd2dcc399f7e06caacf
Uladzislau_Komar <vladkomar25@gmail.com>
2022-10-22 16:43:43 +0300

    Prepared the final version of the MVC

--- Cards/Card.playground/Contents.swift
+++ Cards/Card.playground/Contents.swift
@@ -13,6 +13,7 @@ extension ShapeLayerProtocol {
     }
 }
 
+//MARK: - Frontside of the card (end)
 class CyrcleShape: CAShapeLayer, ShapeLayerProtocol {
     required init(size: CGSize, fillColor: CGColor) {
         super.init()
@@ -63,6 +64,8 @@ class FillShape: CAShapeLayer, ShapeLayerProtocol {
         fatalError("You can't use initial init")
     }
 }
+
+//MARK: - Backside of the card
 
 class BackSideCircle: CAShapeLayer, ShapeLayerProtocol {
     required init(size: CGSize, fillColor: CGColor) {
@@ -117,8 +120,120 @@ class BackSideLines: CAShapeLayer, ShapeLayerProtocol {
     }
 }
 
-class CardView<ShapeType: ShapeLayerProtocol>: UIView {
-   
+//MARK: - Card
+
+protocol FlippableView: UIView {
+    var isFlipped: Bool { get set }
+    var flipCompletionHandler: ((FlippableView) -> Void)? { get set }
+    func flip()
+}
+
+class CardView<ShapeType: ShapeLayerProtocol>: UIView, FlippableView {
+    var color: UIColor!
+    var isFlipped: Bool = false {
+        didSet {
+            self.setNeedsDisplay()
+        }
+    }
+    var flipCompletionHandler: ((FlippableView) -> Void)?
+    private let margin: Int = 10
+    var cornerRadius = 20
+    var startTouchPoint: CGPoint!
+    
+    lazy var frontSideView: UIView = getFrontSideView()
+    lazy var backSideView: UIView = getBackSideView()
+    
+    init(frame: CGRect, color: UIColor) {
+        super.init(frame: frame)
+        self.color = color
+        setupBorders()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        backSideView.removeFromSuperview()
+        frontSideView.removeFromSuperview()
+        
+        if isFlipped {
+            self.addSubview(backSideView)
+            self.addSubview(frontSideView)
+        } else {
+            self.addSubview(frontSideView)
+            self.addSubview(backSideView)
+        }
+    }
+    
+    func flip() {
+        
+    }
+    
+    private func setupBorders() {
+        self.clipsToBounds = true
+        self.layer.cornerRadius = CGFloat(self.cornerRadius)
+        self.layer.borderWidth = 2
+        self.layer.borderColor = UIColor.black.cgColor
+    }
+    
+    func getBackSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        
+        view.backgroundColor = .white
+        
+        switch ["circle", "line"].randomElement() {
+        case "circle":
+            let layer = BackSideCircle(size: self.bounds.size, fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        case "line":
+            let layer = BackSideLines(size: self.bounds.size, fillColor: UIColor.black.cgColor)
+            view.layer.addSublayer(layer)
+        default:
+            break
+        }
+        
+        return view
+    }
+    
+    func getFrontSideView() -> UIView {
+        let view = UIView(frame: self.bounds)
+        view.backgroundColor = .white
+        let shapeView = UIView(frame: CGRect(x: margin,
+                                             y: margin,
+                                             width: Int(self.bounds.width)-margin*2,
+                                             height: Int(self.bounds.height)-margin*2))
+        view.addSubview(shapeView)
+        
+        let shapeLayer = ShapeType(size: shapeView.frame.size, fillColor: color.cgColor)
+        shapeView.layer.addSublayer(shapeLayer as! CALayer)
+        
+        return view
+    }
+    
+    // MARK: Touches
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("first frame \(anchorPoint)")
+        
+        
+        anchorPoint.x = touches.first!.location(in: window).x - self.frame.minX
+        print("first frame \(anchorPoint)")
+        anchorPoint.y = touches.first!.location(in: window).y - self.frame.minY
+        print("first frame \(anchorPoint)")
+        self.startTouchPoint = self.frame.origin
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.frame.origin.x = touches.first!.location(in: window).x - anchorPoint.x
+        self.frame.origin.y = touches.first!.location(in: window).y - anchorPoint.y
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        UIView.animate(withDuration: 0.5) {
+            self.frame.origin = self.startTouchPoint
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 class MyViewController : UIViewController {
@@ -126,7 +241,17 @@ class MyViewController : UIViewController {
         let view = UIView()
         view.backgroundColor = .white
         self.view = view
-        view.layer.addSublayer( BackSideLines(size: CGSize(width: 150, height: 150), fillColor: UIColor.red.cgColor) )
+        
+        let firstCardView = CardView<CyrcleShape>(frame: CGRect(x: 0, y: 0, width: 120, height: 150), color: .red)
+        view.addSubview(firstCardView)
+        
+        let secondCardView = CardView<CyrcleShape>(frame: CGRect(x: 200, y: 0, width: 120, height: 150), color: .red)
+        secondCardView.isFlipped = true
+        view.addSubview(secondCardView)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("controller begin")
     }
 }
 // Present the view controller in the Live View window
